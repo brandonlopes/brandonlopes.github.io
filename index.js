395a1f0aeb93e8777ce27f27718165840a01f377
brandonlopes <brandonnlopes@protonmail.com>
2019-09-04 14:22:17 -0400

    Added book input form

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 let meditations = new Book("Meditations", "Marcus Aurelius", "Philosophy");
 let gameOfThrones = new Book("Game of Thrones", "George R.R Martin", "Fantasy");
 
-
 let myLibrary = [meditations, gameOfThrones];
 
-function Book(title, author, genre){
+function Book(title, author, genre) {
     this.title = title;
     this.author = author;
     this.genre = genre;
-    this.info = function (){
-        return `${this.title} by ${this.author}, ${this.genre}`;
+    this.info = function () {
+        return `${this.title} by ${this.author} - ${this.genre}`;
     }
 }
 
-function addBookToLibrary(){
-    
+function addBookToLibrary() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let genre = document.getElementById("genre").value;
+
+    book = new Book(title, author, genre);
+    myLibrary.push(book);
+    render();
+
 }
 
-function render(){
+function render() {
     let bookList = document.getElementById("bookList");
+    bookList.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
         bookList.innerHTML += `${myLibrary[i].info()} <br><br>`;
     }
 }
 
-render();
+function showForm() {
+    let form = document.getElementById("bookForm")
+    form.style.display = (form.style.display === "none") ? "block" : "none";
+}
