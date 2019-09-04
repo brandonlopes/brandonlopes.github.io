02e9b43c10456c63bffffc064082d138e6a2be3c
brandonlopes <brandonnlopes@protonmail.com>
2019-09-04 11:59:30 -0400

    Render function complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let meditations = new Book("Meditations", "Marcus Aurelius", "Philosophy");
 let gameOfThrones = new Book("Game of Thrones", "George R.R Martin", "Fantasy");
 
+
 let myLibrary = [meditations, gameOfThrones];
 
 function Book(title, author, genre){
@@ -17,8 +18,9 @@ function addBookToLibrary(){
 }
 
 function render(){
+    let bookList = document.getElementById("bookList");
     for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i].info());
+        bookList.innerHTML += `${myLibrary[i].info()} <br><br>`;
     }
 }
 
