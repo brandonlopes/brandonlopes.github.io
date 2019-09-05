700ad93cca1e6dcb9ac0441f5ba5a9a6e097de79
brandonlopes <brandonnlopes@protonmail.com>
2019-09-05 16:02:24 -0400

    Book Form Modal added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let meditations = new Book("Meditations", "Marcus Aurelius", "Philosophy");
 let gameOfThrones = new Book("Game of Thrones", "George R.R Martin", "Fantasy");
 
 let myLibrary = [meditations, gameOfThrones];
+let bookList = document.getElementById("bookList");
 
 function Book(title, author, genre) {
     this.title = title;
     this.author = author;
     this.genre = genre;
+    this.readStatus = false;
     this.info = function () {
         return `${this.title} by ${this.author} - ${this.genre}`;
     }
@@ -17,21 +19,30 @@ function addBookToLibrary() {
     let author = document.getElementById("author").value;
     let genre = document.getElementById("genre").value;
 
-    book = new Book(title, author, genre);
+    let book = new Book(title, author, genre);
     myLibrary.push(book);
     render();
 
 }
 
 function render() {
-    let bookList = document.getElementById("bookList");
+    
     bookList.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
-        bookList.innerHTML += `${myLibrary[i].info()} <br><br>`;
+        createBookCard(myLibrary[i]);
+        // bookList.innerHTML += `${myLibrary[i].info()} <br><br>`;
     }
 }
 
 function showForm() {
-    let form = document.getElementById("bookForm")
+    let form = document.getElementById("bookForm");
     form.style.display = (form.style.display === "none") ? "block" : "none";
+}
+
+function createBookCard(Book){
+    let bookCard = document.createElement("p");
+    bookCard.className = "w3-card";
+    bookCard.innerHTML += `${Book.info()}`;
+    bookList.appendChild(bookCard);
+    document.bobdy.appendChild(bookCard);
 }
