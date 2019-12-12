85020fe05155f4a29b8b483a5f8e535030578ace
brandonlopes <brandonnlopes@protonmail.com>
2019-12-12 15:18:20 -0500

    Added links to books and form validation

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, genre, coverSource, readStatus) {
+function Book(title, author, genre, coverSource, bookSource) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.coverSource = coverSource;
     if (coverSource == null) this.coverSource = "images/booklet.svg";
-    this.readStatus = "Unread";
-    if (readStatus == true) this.readStatus = "Read";
+    this.bookSource = bookSource;
+    // this.readStatus = "Unread";
+    // if (readStatus == true) this.readStatus = "Read";
     this.info = `${this.title}. \n ${this.author}`;
 
 }
@@ -16,8 +17,8 @@ function initializeBooks() {
     if (localStorage.getItem("blowed up")) return;
 
     if (!localStorage.getItem("The Library")) {
-        let meditations = new Book("Meditations", "Marcus Aurelius", "Philosophy", "images/marcusaurelius.jpeg", true);
-        let artOfWar = new Book("The Art of War", "Sun Tzu", "Translated by Thomas Cleary", "images/artofwar.jpeg");
+        let meditations = new Book("Meditations", "Marcus Aurelius", "Philosophy", "images/marcusaurelius.jpeg", "books/Meditations - Marcus Aurelius.html");
+        let artOfWar = new Book("The Art of War", "Sun Tzu", "Translated by Thomas Cleary", "images/artofwar.jpeg", "books/The Art of War, by Sun Tzu.html");
         myLibrary.push(meditations, artOfWar);
     } else myLibrary = JSON.parse(localStorage.getItem("The Library"));
 }
@@ -40,21 +41,18 @@ function createEventListeners() {
     newBookButton.addEventListener("click", function () {
         toggleDisplay("bookForm");
         toggleDisplay("newBookButton");
-        toggleClearStorageButton();
         clearInputFields();
     });
 
     document.getElementById("addBookButton").addEventListener("click", function () {
         addBookToLibrary();
-        toggleClearStorageButton();
-        toggleDisplay("bookForm");
+        // toggleDisplay("bookForm");
         toggleDisplay("clearStorage")
     });
 
     let closeButton = document.getElementById("closeButton");
     closeButton.addEventListener("click", function () {
         toggleDisplay("bookForm");
-        toggleClearStorageButton();
     });
 }
 
@@ -70,21 +68,33 @@ function addBookToLibrary() {
     let author = document.getElementById("author").value;
     let genre = document.getElementById("genre").value;
 
-    let book = new Book(title, author, genre);
+    let formError = document.getElementById("formError");
 
-    let imageRegex = new RegExp("(.html)$")
-    if (!imageRegex.exec(previewImg.src)) book.coverSource = previewImg.src;
+    if (title && author) {
+        formError.innerText = "";
+        let book = new Book(title, author, genre);
 
-    myLibrary.push(book);
-    updateLocalStorage();
-    render();
+        let imageRegex = new RegExp("(.html)$")
+        if (!imageRegex.exec(previewImg.src)) book.coverSource = previewImg.src;
+
+        myLibrary.push(book);
+        updateLocalStorage();
+        toggleDisplay("bookForm");
+        render();
+    }
+
+    else formError.innerText = "Books must have a title and an author";
 }
 
 function render() {
     bookList.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
-        createBookCard(myLibrary[i], "bookList")
+        createBookCard(myLibrary[i], "bookList");
+        toggleClearStorageButton();
+
     }
+    console.log(myLibrary.length);
+
 }
 
 function createBookCard(Book, container) {
@@ -113,14 +123,17 @@ function createBookCard(Book, container) {
         deleteBook(Book);
     });
 
+    let bookLink = document.createElement("a");
+    bookLink.setAttribute("href", Book.bookSource);
     let readBookButton = document.createElement("img");
     readBookButton.src = "images/openbook.svg";
     readBookButton.setAttribute("class", "icons");
     readBookButton.style.left = "0";
+    bookLink.appendChild(readBookButton);
 
 
     bookInfo.appendChild(bookInfoText);
-    bookInfo.appendChild(readBookButton);
+    bookInfo.appendChild(bookLink);
     bookInfo.appendChild(deleteBookButton);
     bookCard.appendChild(bookInfo);
     document.getElementById(container).appendChild(bookCard);
@@ -143,8 +156,17 @@ function toggleVisibility(elementID) {
 }
 
 function toggleClearStorageButton() {
-    if (localStorage.getItem("blowed up") || myLibrary.length === 0) document.getElementById("clearStorage").style.display = "none";
-    else document.getElementById("clearStorage").style.display = "block";
+    let bomb = document.getElementById("clearStorage");
+    if (myLibrary.length > 0) {
+        console.log(`There are things to blow up. Bomb should be there`);
+
+        bomb.style.display = "block";
+    }
+    else {
+        console.log(`The library is empty so the bomb should be gone`);
+
+        bomb.style.display = "none";
+    }
 }
 
 function toggleClass(elementID, classID) {
